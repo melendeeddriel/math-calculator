5e14c9564f688c3e8bb71f45c195b666306fe7c6
melendeeddriel <melendeeddriel@gmail.com>
2025-03-07 08:17:10 +0000

    Add 2.py

---
+++ src/2.py
@@ -0,0 +1,10 @@
+
+def calculate(num1, num2, operation):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    else:
+        return "Invalid operation"
