1630e4f274b32577dc2ceba30e9bd9db0056d129
melendeeddriel <melendeeddriel@gmail.com>
2025-03-08 05:29:55 +0000

    Add 3.py

---
+++ src/3.py
@@ -0,0 +1,11 @@
+def calculate_math(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        raise ValueError("Invalid operator")
