9ff4694a7c3b1d24ad2d633f583681b470ed5214
melendeeddriel <melendeeddriel@gmail.com>
2025-03-19 10:45:08 +0000

    Add 13.py

---
+++ src/13.py
@@ -0,0 +1,11 @@
+def calculate(num1, num2, op):
+    if op == "+":
+        return num1 + num2
+    elif op == "-":
+        return num1 - num2
+    elif op == "*":
+        return num1 * num2
+    elif op == "/":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        return num1 / num2
