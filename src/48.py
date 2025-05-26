0c1a6f96e582cf8c63a9cbb0e08ef48c176a0700
melendeeddriel <melendeeddriel@gmail.com>
2025-05-26 19:14:20 +0000

    Add 48.py

---
+++ src/48.py
@@ -0,0 +1,15 @@
+import math
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    else:
+        return x / y
